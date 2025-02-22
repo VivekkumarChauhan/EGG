f5351cbbeb77efc7fcdcaff417efe38973a209bc
Vivekkumar Chauhan <147121528+VivekkumarChauhan@users.noreply.github.com>
2025-02-22 12:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
 model = joblib.load('model_rf.pkl')
-scaler = StandardScaler()
 
 prediction_map = {0: 'Healthy', 1: 'Epilepsy', 2: 'Alzheimer\'s'}
 
@@ -18,16 +16,17 @@ def home():
 def predict():
     try:
         data = request.get_json(force=True)
+
         features = np.array([data['Mean_Amplitude'], data['Peak_to_Peak'], data['PSD'], 
                              data['Entropy'], data['Delta_Power'], data['Theta_Power'], 
                              data['Alpha_Power'], data['Beta_Power'], data['Gamma_Power']]).reshape(1, -1)
-        
-        features_scaled = scaler.fit_transform(features)
-        prediction = model.predict(features_scaled)
+
+        prediction = model.predict(features)
+
         predicted_condition = prediction_map.get(int(prediction[0]), "Unknown")
-        
+
         return jsonify({'prediction': predicted_condition})
-    
+
     except Exception as e:
         return jsonify({'error': str(e)})
 
